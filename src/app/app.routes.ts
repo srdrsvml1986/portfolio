b63b62d206c4cde45774e76a4c0a128ce8630de9
Serdar Sevimli <srdrsvml1986@gmail.com>
2025-02-25 22:05:18 +0300

    Update personal website content and routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,61 +1,11 @@
 import { Routes } from '@angular/router';
-import { BlankComponent } from './layouts/blank/blank.component';
-import { FullComponent } from './layouts/full/full.component';
 import { HomeComponent } from './components/home/home.component';
+import { ArchiveComponent } from './components/archive/archive.component';
 
 export const routes: Routes = [
-  {
-    path: ':language',
-    component: HomeComponent,
-    children: [
-      {
-        path: '',
-        component: HomeComponent
-      }
-    ]
-  },
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: '',
-    component: FullComponent,
-    children: [
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./pages/pages.routes').then((m) => m.PagesRoutes),
-      },
-      {
-        path: 'ui',
-        loadChildren: () =>
-          import('./pages/ui-components/ui-components.routes').then(
-            (m) => m.UiComponentsRoutes
-          ),
-      },
-      {
-        path: 'extra',
-        loadChildren: () =>
-          import('./pages/extra/extra.routes').then((m) => m.ExtraRoutes),
-      },
-    ],
-  },
-  {
-    path: '',
-    component: BlankComponent,
-    children: [
-      {
-        path: 'auth',
-        loadChildren: () =>
-          import('./pages/authentication/authentication.routes').then(
-            (m) => m.AuthenticationRoutes
-          ),
-      },
-    ],
-  },
-  {
-    path: '**',
-    redirectTo: '',
-  },
+  {path: '', component: HomeComponent},
+  {path: 'tr', component: HomeComponent},
+  {path: 'en', component: HomeComponent},
+  {path: ':language/proyectos', component: ArchiveComponent},
+  {path: '**', pathMatch: 'full', redirectTo: '/'},
 ];
