95bdc64726b1f3567792b3f3840daba2d1aa7790
Serdar Sevimli <srdrsvml1986@gmail.com>
2025-02-26 23:18:22 +0300

    Modernize project configuration and styling with responsive design updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { ArchiveComponent } from './components/archive/archive.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'tr', component: HomeComponent},
-  {path: 'en', component: HomeComponent},
-  {path: ':language/proyectos', component: ArchiveComponent},
-  {path: '**', pathMatch: 'full', redirectTo: '/'},
+  {path: '', redirectTo: '/tr', pathMatch: 'full'},
+  {
+    path: ':language',
+    children: [
+      {path: '', component: HomeComponent},
+    ]
+  },
+  {path: '**', redirectTo: ''},
 ];
